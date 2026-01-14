cae34d7aff12862f7f3198054ce7aadc9bdc03be
customerss Juan Carlos Limon <customerss@gv.local>
2026-01-14 12:56:12 -0600

    Add production env to angular configs for compilation

--- FE/src/environments/environment.prod.ts
+++ FE/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://192.168.190.140:401',
+  apiBaseUrl: 'https://apisvanguardia.com:401',
   vanguardia: {
     apiUrl: 'https://apisvanguardia.com:400/vgd/singlefilecustomer',  // Llamada directa a Vanguardia
     ordersApiUrl: 'https://apisvanguardia.com:400/vgd/singlefileorders',  // Llamada directa a Vanguardia
