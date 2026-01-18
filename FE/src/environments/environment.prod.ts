0a7b9d0f2da9a8ebf66a87bea33b5d5eaf8be5d2
JcLimonero <limon2633@gmail.com>
2026-01-18 13:21:05 -0600

    fix url deploy

--- FE/src/environments/environment.prod.ts
+++ FE/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   vanguardia: {
     apiUrl: 'https://apisvanguardia.com:400/vgd/singlefilecustomer',  // Llamada directa a Vanguardia
     ordersApiUrl: 'https://apisvanguardia.com:400/vgd/singlefileorders',  // Llamada directa a Vanguardia
-    uploadApiUrl: 'http://192.168.190.140:401/api/backblaze/upload'  // Usa proxy backend para agregar X-Provider-Token
+    uploadApiUrl: 'https://apisvanguardia.com:400/backblaze/upload'  // Usa proxy backend para agregar X-Provider-Token
   }
 };
