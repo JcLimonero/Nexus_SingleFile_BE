ed801081933d4d93526e9f2e13203a3bd6c18b95
JcLimonero <limon2633@gmail.com>
2026-03-05 18:14:17 -0600

    fix routes

--- DWH/app/Filters/CorsFilter.php
+++ DWH/app/Filters/CorsFilter.php
@@ -28,14 +28,8 @@ class CorsFilter implements FilterInterface
         if ($response->hasHeader('Access-Control-Allow-Origin')) {
             return;
         }
-        $origin = $request->getHeaderLine('Origin');
-        $allowed = ['http://localhost:3600', 'http://localhost:4200', 'http://localhost:402'];
-        if ($origin && (in_array($origin, $allowed, true) || preg_match('#\Ahttp://(localhost|127\.0\.0\.1):\d+\z#', $origin))) {
-            $response->setHeader('Access-Control-Allow-Origin', $origin);
-        } else {
-            $response->setHeader('Access-Control-Allow-Origin', '*');
-        }
-        $response->setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+        $response->setHeader('Access-Control-Allow-Origin', '*');
+        $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, X-Provider-Token');
         $response->setHeader('Access-Control-Max-Age', '7200');
     }
